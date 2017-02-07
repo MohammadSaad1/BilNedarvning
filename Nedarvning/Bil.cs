b61989d055c4a7deff26a2e24a42b8efc1c7f5fb
Sarasaan Sivaguru <sara360e@edu.easj.dk>
2017-02-07 09:23:22 +0100

    Add project files.

---
+++ Nedarvning/Bil.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nedarvning
+{
+    public class Bil
+    {
+        public int BilPrisExAfgift { get; private set; }
+        public int KøbsÅr { get; private set; }
+        public string Mærke { get; set; }
+        public string RegistreringsNr { get; set; }
+        public int KmPrLiter { get; set; }
+
+
+
+
+
+        public virtual double RegistreringsAfgift()
+        {
+            if (KøbsÅr <= 2014)
+            {
+                double RegistreringsAfgift = ((1.05 * 80500) + (1.80 * (BilPrisExAfgift - 80500)));
+                return RegistreringsAfgift;
+            }
+
+            else if (KøbsÅr >= 2015)
+            {
+                double RegistreringsAfgift = ((1.05 * 80500) + (1.80 * (BilPrisExAfgift - 81700)));
+                return RegistreringsAfgift;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+       
+
+        public double TotalPris()
+        {
+            return BilPrisExAfgift + RegistreringsAfgift();
+        }
+
+        public virtual int HalvÅrligEjerafgift()
+        {
+            return 1000;
+        }
+
+        public Bil (string Mærke, int PrisExAfgift, int KøbsÅr, int KmPrLiter)
+        {
+            this.Mærke = Mærke;
+            this.BilPrisExAfgift = BilPrisExAfgift;
+            this.KøbsÅr = KøbsÅr;
+            this.KmPrLiter = KmPrLiter;
+        }
+
+        
+
+
+    }
+}
---
+++ Nedarvning/DieselBil.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nedarvning
+{
+    public class DieselBil : Bil
+    {
+        public bool PartikelFilter { get; private set; }
+
+
+        public override double RegistreringsAfgift()
+        {
+            return base.RegistreringsAfgift();
+        }
+
+    }
+}
